1d9d2325add1b5563b68048554da7db8fb46b752
Christopher Luk <97375770+christopherjluk@users.noreply.github.com>
2023-06-21 11:31:07 -0400

    Add "Hello World" microcontroller file
    Initial hello world file

---
+++ one_led_blink.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(9, INPUT);
+  pinMode(8, OUTPUT);
+}
+
+void loop() {
+  while(digitalRead(9) == HIGH){
+    digitalWrite(8, HIGH);
+    delay(75); // Not sure why, but a delay of 1000 gives me a delay of around 20ish seconds
+    digitalWrite(8, LOW);
+    delay(75); // Same as before
+  }
+}
