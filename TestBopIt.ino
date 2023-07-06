18af70bed11b13e49489b2e6f3b33d2cf1503da1
Christopher Luk <97375770+christopherjluk@users.noreply.github.com>
2023-07-06 17:21:07 -0400

    Update TestBopIt.ino

--- TestBopIt.ino
+++ TestBopIt.ino
@@ -2,16 +2,19 @@
 
 int randNumber = 0;
 int score_counter = 0;
-long timer_delay = 0;
+long initial_time = 0;
+long running_timer = 0;
+bool timer_up = false;
 bool runner = false;
-long analog_button = 0;
+bool inputSelected = false;
+bool inputCorrect = false;
 long analog_slide = 0;
 int analog_rotary = 0;
 
 void setup() {
   pinMode(A4, INPUT); //Button
   pinMode(A0, INPUT); //Slider
-  pinMode(5, INPUT); //Rotary
+  pinMode(A5, INPUT); //Rotary
   pinMode(0, OUTPUT); //O1
   pinMode(1, OUTPUT); //O2
   pinMode(2, OUTPUT); //O3
@@ -29,56 +32,141 @@ void setup() {
 }
 
 void loop() {
-  if(runner == false){
+  if(runner == false){ // If statement for starting a new game
     hex_output(0);
-    delay(1000);
+    initial_time = 5000;
     runner = true;
   }
   while(runner == true){
-    hex_output(2);
+    randNumber = random(1, 4);
+    delay(1000);
+    analog_slide = analogRead(0);
     analog_rotary = analogRead(5);
-    analog_rotary = map(analog_rotary, 0, 1023, 0, 255);
-    delay(5000);
-    if(abs(map(analogRead(5), 0, 1023, 0, 255) - analog_rotary) > 50){
-      hex_output(5);
-      delay(1000);
+    if(randNumber == 1){
+      inputSelected = false;
+      running_timer = initial_time;
+      timer_up = false;
+      tone(9, 2000);
+      digitalWrite(8, HIGH); // For testing purposes
+      delay(2000);
+      noTone(9);
+      while(!timer_up && !inputSelected){
+        delay(40);
+        if(analogRead(4) > 200){
+          inputSelected = true;
+          inputCorrect = true;
+        }
+        else if(abs(analog_slide - analogRead(0)) > 50){
+          inputSelected = true;
+          inputCorrect = false;
+        }
+        else if(abs(analog_rotary - analogRead(5)) > 50){
+          inputSelected = true;
+          inputCorrect = false;
+        }
+        else{
+          running_timer -= 40;
+          if(running_timer == 0)
+            timer_up = true;
+        }
+      }
+      digitalWrite(8, LOW); // For testing purposes
+      if(inputSelected and inputCorrect)
+        correct_input();
+      else
+        wrong_input();
     }
-    else{
-      hex_output(3);
-      delay(1000);
+    else if(randNumber == 2){
+      inputSelected = false;
+      running_timer = initial_time;
+      tone(9, 1000);
+      digitalWrite(8, HIGH); // For testing purposes
+      delay(2000);
+      noTone(9);
+      while(!timer_up && !inputSelected){
+        delay(40);
+        if(abs(analog_slide - analogRead(0)) > 50){
+          inputSelected = true;
+          inputCorrect = true;
+        }
+        else if(analogRead(4) > 200){
+          inputSelected = true;
+          inputCorrect = false;
+        }
+        else if(abs(analog_rotary - analogRead(5)) > 50){
+          inputSelected = true;
+          inputCorrect = false;
+        }
+        else{
+          running_timer -= 40;
+          if(running_timer == 0)
+            timer_up = true;
+        }
+      }
+      digitalWrite(8, LOW); // For testing purposes
+      if(inputSelected and inputCorrect)
+        correct_input();
+      else
+        wrong_input();
+    }
+    else if(randNumber == 3){
+      inputSelected = false;
+      running_timer = initial_time;
+      timer_up = false;
+      tone(9, 500);
+      digitalWrite(8, HIGH); // For testing purposes
+      delay(2000);
+      noTone(9);
+      while(!timer_up && !inputSelected){
+        delay(40);
+        if(abs(analog_rotary - analogRead(5)) > 50){
+          inputSelected = true;
+          inputCorrect = true;
+        }
+        else if(analogRead(4) > 200){
+          inputSelected = true;
+          inputCorrect = false;
+        }
+        else if(abs(analog_slide - analogRead(0)) > 50){
+          inputSelected = true;
+          inputCorrect = false;
+        }
+        else{
+          running_timer -= 40;
+          if(running_timer == 0)
+            timer_up = true;
+        }
+      }
+      digitalWrite(8, LOW); // For testing purposes
+      if(inputSelected and inputCorrect)
+        correct_input();
+      else
+        wrong_input();
     }
   }
-}
-
-void slide_toggler() {
-  tone(9, 500);
-  score_counter = score_counter + 1;
-  hex_output(score_counter);
-}
-
-void rotary_toggler() {
-  tone(9, 1000);
-  score_counter = score_counter + 1;
-  hex_output(score_counter);
 }
 
 void correct_input() {
   ++score_counter;
   hex_output(score_counter);
+  delay(3000); // For testing purposes
   // Make LED green
   if(score_counter == 99){
-    // Announce game over
-    // Delay for 10 seconds
+    // Announce game over (and possibly delay)
+    game_end();
   }
-  else{
-    // Decrement timer
-  }
+  else
+    initial_time -= 40;
 }
 
 void wrong_input() {
-  runner = false;
+  // Announce game over (and possibly delay)
   // Make LED red
-  // Delay for 10 seconds
+  game_end();
+}
+
+void game_end() {
+  while(true){}
 }
 
 void hex_output(int score) {
